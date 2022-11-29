f09af4638446a4cae86d8e1c9acce24c4a75665f
Evandro <evandro.souzac@gmail.com>
2022-11-28 22:42:04 -0300

    Inserindo animação - LifeStatus e estruturando componentes

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Routes from "./src/routes";
 export default function App() {
   return (
     <>
-      <StatusBar barStyle={'auto'} />
+      <StatusBar barStyle={'auto'} translucent={true}/>
       <Routes />
     </>
   );
