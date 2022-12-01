fd94e7fad954432dc61c7c07193178ac05bf95cc
Evandro <evandro.souzac@gmail.com>
2022-11-30 21:09:41 -0300

    Botão de Criar Hábitos

---
+++ src/components/Home/CreateHabit/index.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import {Text, StyleSheet, TouchableOpacity } from "react-native";
+
+export default function CreateHabit({ habitArea, borderColor }) {
+
+    function handleCreate() {
+        console.log(`Botão da área clicado: ${habitArea}`)
+    }
+
+    return (
+        <TouchableOpacity
+            activeOpacity={0.8}
+            style={[styles.buttom, { borderColor: borderColor }]}
+            onPress={handleCreate}
+            >
+                <Text style={styles.habitTitle}>
+                    Adicionar meta { habitArea === "Mente" ? 'da' : 'do' } {habitArea}
+                </Text>
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    buttom: {
+        width: 315,
+        marginVertical: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderWidth: 2,
+        borderStyle: "dotted",
+        borderColor: "white",
+        borderRadius: 5,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+
+    habitTitle: {
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold'
+    },
+});
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { View, Text, ScrollView, StyleSheet } from "react-native";
 
 import LifeStatus from "../../components/common/LifeStatus";
 import StatusBar from "../../components/Home/StatusBar";
+import CreateHabit from "../../components/Home/CreateHabit";
 
 
 export default function Home() {
     const navigation = useNavigation();
+    const [mindHabit, setMindHabit] = useState();
+    const [moneyHabit, setMoneyHabit] = useState();
+    const [bodyHabit, setBodyHabit] = useState();
+    const [funHabit, setFunHabit] = useState();
 
     function handleNavExplanation() {
         navigation.navigate("AppExplanation")
@@ -21,6 +26,7 @@ export default function Home() {
 
                     <LifeStatus />
                     <StatusBar />
+                    <CreateHabit habitArea="Mente" borderColor="#90B7F3" />
                 </View>
                 
                 <Text 
